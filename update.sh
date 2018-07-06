209e9d70ab82c0bc8e6e1110e342705b327ea859
Yogesh Swami <axelexic@gmail.com>
2018-07-06 12:34:00 -0700

    Last DNS update at Fri Jul  6 12:34:00 PDT 2018

--- update.sh
+++ update.sh
@@ -3,11 +3,16 @@ set -x
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+LOG_FILE="$THIS_DIR/update.log"
+
 git_msg ( ) {
     printf "Last DNS update at %s" "$(date)"
 }
 
-curl -s -o "$THIS_DIR/update.txt" "http://www.myexternalip.com/raw" && \
-    git -C "$THIS_DIR" add  "$THIS_DIR/update.txt" && \
-    git -C "$THIS_DIR" commit -m "$(git_msg)"      && \
-    git -C "$THIS_DIR" push origin master
+echo "\n==== Invoking CRON at $(date) ==== \n" > "$LOG_FILE" 2>&1
+
+curl -s -o "$THIS_DIR/update.txt" "http://www.myexternalip.com/raw"  > "$LOG_FILE" 2>&1 && \
+    git -C "$THIS_DIR" add  "$THIS_DIR/update.txt"  > "$LOG_FILE" 2>&1 && \
+    git -C "$THIS_DIR" commit -m "$(git_msg)"  > "$LOG_FILE" 2>&1
+
+git -C "$THIS_DIR" push origin master
