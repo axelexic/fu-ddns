1d731bc9ca181818647b6bda2f590fda15ed2f00
Yogesh Swami <axelexic@gmail.com>
2018-07-06 12:36:56 -0700

    Last DNS update at Fri Jul  6 12:36:56 PDT 2018

--- update.sh
+++ update.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -x
-
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LOG_FILE="$THIS_DIR/update.log"
@@ -9,10 +7,10 @@ git_msg ( ) {
     printf "Last DNS update at %s" "$(date)"
 }
 
-echo "\n==== Invoking CRON at $(date) ==== \n" > "$LOG_FILE" 2>&1
+echo "==== Invoking CRON at $(date) ==== " >> "$LOG_FILE" 2>&1
 
-curl -s -o "$THIS_DIR/update.txt" "http://www.myexternalip.com/raw"  > "$LOG_FILE" 2>&1 && \
-    git -C "$THIS_DIR" add  "$THIS_DIR/update.txt"  > "$LOG_FILE" 2>&1 && \
-    git -C "$THIS_DIR" commit -m "$(git_msg)"  > "$LOG_FILE" 2>&1
+curl -s -o "$THIS_DIR/update.txt" "http://www.myexternalip.com/raw"  >> "$LOG_FILE" 2>&1 && \
+    git -C "$THIS_DIR" add  "$THIS_DIR/update.txt"  >> "$LOG_FILE" 2>&1 && \
+    git -C "$THIS_DIR" commit -m "$(git_msg)"  >> "$LOG_FILE" 2>&1
 
-git -C "$THIS_DIR" push origin master
+git -C "$THIS_DIR" push origin master >> "$LOG_FILE" 2>&1
