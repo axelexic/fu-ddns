a6a61a13ad59ca35cf5ab8f25c17e91bdbd5c682
Yogesh Swami <axelexic@gmail.com>
2018-07-06 12:10:08 -0700

    Last DNS update at Fri Jul  6 12:10:08 PDT 2018

--- update.sh
+++ update.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+set -x
 
-curl "http://www.myexternalip.com/raw" > update.txt
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+git_msg ( ) {
+    printf "Last DNS update at %s" "$(date)"
+}
+
+curl -s -o "$THIS_DIR/update.txt" "http://www.myexternalip.com/raw" && \
+    git -C "$THIS_DIR" add  "$THIS_DIR/update.txt" && \
+    git -C "$THIS_DIR" commit -m "$(git_msg)"      && \
+    git -C "$THIS_DIR" push origin master
